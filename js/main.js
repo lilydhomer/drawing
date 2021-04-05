f0fec0d6771c9c0e6fa9e9f5f26af9e7a9a9bd77
Lily Homer <lilydhomer@gmail.com>
2021-04-05 11:26:37 -0400

    remove notes

--- js/main.js
+++ js/main.js
@@ -4,14 +4,12 @@ background(255)
 }
 
 function draw () {
-  let x = mouseX//width / 2
-  let y = mouseY //height / 2
+  let x = mouseX
+  let y = mouseY
   let d = width / 18
-// val, minVal, maxVal, newMin, newMax
   let r = map(mouseX, 0, width, 0, 255)
   fill(r, 0, 0)
   if (mouseIsPressed) {
     ellipse(x, y, d)
   }
-
 }
